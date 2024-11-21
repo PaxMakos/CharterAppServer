15b69717a7aba385c98fedc72af7a16a3ff94e64
Pakos <pakostasip@gmail.com>
2024-11-22 00:36:47 +0100

    Added models to admin site

--- ChartersDatabase/admin.py
+++ ChartersDatabase/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Port, Boat, Charter
 
-# Register your models here.
+
+admin.site.register(Port)
+admin.site.register(Boat)
+admin.site.register(Charter)
