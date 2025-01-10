8218e4f615178e9575fad03860385e46d46c2cb4
Pakos <pakostasip@gmail.com>
2025-01-10 19:21:29 +0100

    Added photo model to admin panel

--- ChartersDatabase/admin.py
+++ ChartersDatabase/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
-from .models import Port, Boat, Charter
-
+from .models import Port, Boat, Charter, Photo
 
 admin.site.register(Port)
 admin.site.register(Boat)
 admin.site.register(Charter)
+admin.site.register(Photo)
