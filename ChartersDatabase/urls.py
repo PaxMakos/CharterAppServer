f68e7c5bf32fa027041ed719703ed5c0759c82ed
Wojciech-Krol <wojtekkrool@gmail.com>
2025-01-12 14:24:29 +0100

    small changes, getAllBoats gets all infos of boats, getAllBoatsNames returns all boats names

--- ChartersDatabase/urls.py
+++ ChartersDatabase/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     path('register/', authViews.register),
     path('boats/', boatViews.getAllBoats),
     path('boats/byPort/', boatViews.getBoatsByPort),
+    path('boats/names', boatViews.getAllBoats),
     path('boats/byCompany/', boatViews.getBoatsByCompany),
     path('boats/details/', boatViews.getBoat),
     path('charters/ByBoat/', charterViews.getChartersByBoat),
