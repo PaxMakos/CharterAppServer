29f62c4b482bd34a988e3797540151f8ef61333f
Pakos <pakostasip@gmail.com>
2025-01-12 13:20:15 +0100

    Added email to user profile

--- ChartersDatabase/views/authViews.py
+++ ChartersDatabase/views/authViews.py
@@ -39,11 +39,12 @@ def register(request):
     try:
         username = request.POST['username']
         password = request.POST['password']
+        email = request.POST['email']
     except KeyError:
         return JsonResponse({'status': 'error', 'message': 'Missing username or password'})
 
     try:
-        user = User.objects.create_user(username=username, password=password)
+        user = User.objects.create_user(username=username, password=password, email=email)
         user.save()
         return JsonResponse({'status': 'success'})
     except ValidationError:
